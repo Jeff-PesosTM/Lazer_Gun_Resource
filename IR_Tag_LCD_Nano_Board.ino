9eb2b63206a2a2473247deb7115cac8ed5d6cd56
Jeff-PesosTM <153344919+Jeff-PesosTM@users.noreply.github.com>
2025-01-23 09:38:54 -0600

    print files and code

---
+++ IR_Tag_LCD_Nano_Board.ino
@@ -0,0 +1,126 @@
+//https://docs.arduino.cc/learn/electronics/lcd-displays/
+//LCD brightness potentiometer (VO pin) = 4k7 ohm resistor (@ 5V power)
+//https://www.thingiverse.com/thing:4207521
+//https://www.thingiverse.com/thing:3146046
+//https://www.thingiverse.com/thing:3658610 (To Do After Maybe)
+//https://hackaday.io/project/160804-lzrtag-flexible-diy-lasertag
+
+//IRremote library is reverted to v2.5
+
+#include <IRremote.h>
+
+IRsend irsend;
+
+const int ledPin = 6;  // the number of the LED pin 
+const int RECV_PIN = 2;
+const int buzzerPin = 5;
+
+IRrecv irrecv(RECV_PIN);  //passive?
+
+const int triggpin = 13;
+const int amoresetpin = 12;
+
+int triggbuttonState = 0;  // = LOW;
+int amobuttonState = 0;    // = LOW;
+int damagelevel = 10;
+int maxamo = 1000;  // maximum amount of ammo the player can have
+int amolevel = maxamo;
+int timer = 10;
+int reloading = 30;       //  ~ 1s / 10 increments
+int reload_time = 30;
+
+decode_results results;
+
+void setup() {
+
+  pinMode(triggpin, INPUT);     // to shoot, minus amo
+  pinMode(amoresetpin, INPUT);  //resets amo, what is the coast?
+  pinMode(buzzerPin, OUTPUT);   // output for shooting,gettingshot and game over
+  pinMode(ledPin, OUTPUT);      //output for shooting with LED muzzle?
+  pinMode(RECV_PIN, INPUT);     //getting shot, taking damage
+
+  Serial.begin(9600);
+  irrecv.enableIRIn();  // Start the receiver
+}
+
+void loop() {
+
+  triggbuttonState = digitalRead(triggpin);
+  amobuttonState = digitalRead(amoresetpin);
+
+  debug();
+  reload();
+  shoot();
+  gotShot();
+  reloading++;
+  timer++;
+}
+
+void reload() {
+  if (amolevel < maxamo && damagelevel > 0) {
+    if (amobuttonState == HIGH) {
+      reloading = 0;
+      amolevel = maxamo;
+    }
+  }
+}
+
+void debug() {
+  delay(100);
+  Serial.print(timer);
+  Serial.print(" amobuttonState ");
+  Serial.print(amobuttonState);
+  Serial.print(" amocount ");
+  Serial.print(amolevel);
+  Serial.print(" damage level ");
+  Serial.println(damagelevel);
+ // Serial.print(irrecv.decode());
+}
+
+void shoot() {
+  if ((triggbuttonState == HIGH) && (amolevel > 0) && (damagelevel > 0) && (reloading > reload_time)) {
+    timer = 11;
+    amolevel--;
+    IRsend irsend;
+    irsend.sendSony(0xa90, 20);
+    delay(1);
+
+    for (int i = 500; i < 600; i++) {  //make some noise
+      digitalWrite(buzzerPin, HIGH);
+      delayMicroseconds(i);
+      digitalWrite(buzzerPin, LOW);
+      delayMicroseconds(i);
+    }
+
+    for (int i = 500; i < 600; i++) {  
+      digitalWrite(ledPin, HIGH);
+      delayMicroseconds(i);
+      digitalWrite(ledPin, LOW);
+      delayMicroseconds(i);
+    }
+
+    irrecv.enableIRIn();
+    delay(100);
+    Serial.println("triggbuttonState HIGH");
+  }
+}
+
+void gotShot() {
+  if (irrecv.decode(&results)) {
+    //Serial.println(results.value, HEX);
+    damagelevel=damagelevel-1;
+    if (damagelevel < 1) {
+      damagelevel = 0; 
+      amolevel = 0;
+    }
+    
+
+    for(int i = 200; i < 400; i++){
+      digitalWrite(buzzerPin, HIGH);
+      delayMicroseconds(i);
+      digitalWrite(buzzerPin, LOW);
+      delayMicroseconds(i);
+    }
+    irrecv.resume();
+  }
+}
